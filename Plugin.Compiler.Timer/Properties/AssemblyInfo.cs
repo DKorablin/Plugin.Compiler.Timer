a97b69bc6d6b2e14b91ba4c9e2d21e41f047b809
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 17:44:46 +0300

    Unify plugins deployment

--- Plugin.Compiler.Timer/Properties/AssemblyInfo.cs
+++ Plugin.Compiler.Timer/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("d7a9a565-db2b-458e-bbb4-4ab1e8bb33bf")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=120")]
 #else
 
-[assembly: AssemblyTitle("Plugin.Compiler.Timer")]
 [assembly: AssemblyDescription("Create runtime method and invoke it in specific interval")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.CompilerTimer")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2019-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2019-2025")]
 #endif
